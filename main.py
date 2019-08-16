c3c49043962ac7d33c6b21ba454fabb6e1641ba0
Romain <romain.v@live.be>
2019-08-16 14:19:14 +0200

    animeName = episode nbr parsed

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 from bs4 import BeautifulSoup
+import requests
 
+def parseHTML():
+    resp = requests.get("https://www.crunchyroll.com/simulcastcalendar?filter=free")
+    link = resp.text
+    html = BeautifulSoup(link,'lxml')
+    articles = html.find_all('article')
+    for article in articles :
+        if article.attrs != None :
+            attr = article.attrs
+            if "data-slug" in attr :
+                animeName = attr["data-slug"]
+                animeName = animeName.replace('-',' ')
+                epNbr = int(attr["data-episode-num"]) + 1
+                print(animeName+ " " + str(epNbr) )
 
-def parseHTML(link):
-    print("Crunchyroll")
-    pass
+if __name__ == '__main__':
+    parseHTML()
