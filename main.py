5d559cfb25710c5ec9618c8609200b7f159cb3e2
Romain <romain.v@live.be>
2019-08-18 14:32:13 +0200

    Correction availability condition + proxy US to get full list

--- main.py
+++ main.py
@@ -8,8 +8,13 @@ animes = []
 day = datetime.today().weekday()
 dayDict = {0 : "Monday",1:"Tuesday",2:"Wednesday",3:"Thrusday",4:"Friday",5:"Saterday",6:"Sunday"}
 
+proxies = {
+  'http': 'http://162.252.96.65:8080', #GMT-9 9 hours less than here
+  'https': 'http://34.94.147.20:3128',
+}
+
 def parseHTML():
-    resp = requests.get("https://www.crunchyroll.com/simulcastcalendar?filter=free")
+    resp = requests.get("https://www.crunchyroll.com/simulcastcalendar?filter=free",proxies=proxies)
     link = resp.text
     html = BeautifulSoup(link,'lxml')
     articles = html.find_all('article')
@@ -31,28 +36,35 @@ def parseHTML():
                 h = time.text
                 suffix = h[len(h)-2:]
                 h = h[:len(h)-2].split(':')
+                h = [int(h[0]),int(h[1])]
                 #print(h,suffix) 
-                if(suffix == "pm" and h[0]!="12") :
-                    h[0] = int(h[0])+12
+                if(suffix == "pm" and h[0]!=12) :
+                    h[0] =  int(h[0])+12 
+                offset = 0
+                h[0] += 9
+                if(h[0] >= 24) :
+                    h[0] = h[0]%24
+                    offset = 1
                 animes[count].append(int(h[0]))
                 animes[count].append(int(h[1]))
+
                 date = attr['datetime'][:10].split("-") #format YYYY-MM-DD
-                day = datetime(int(date[0]),int(date[1]),int(date[2])).weekday()
+                day = ( datetime(int(date[0]),int(date[1]),int(date[2])).weekday() + offset ) %7
                 animes[count].append(day)
                 animes[count].append(False)
                 count+=1
-    print(count)
-
-
+    
+    
 def checkrelease() :
     todayAnimes = []
     for anime in animes :
         if anime[4] == day :
+            print(anime[0] + " " + str(anime[2]) + ":" + str(anime[3]) + " day " + dayDict[anime[4]] )
             todayAnimes.append(anime)
     while True :
         now = datetime.now()
         for anime in todayAnimes :
-            if(anime[2] <= now.hour and anime[3] <= now.minute and anime[5] == False) :
+            if( ( anime[2] < now.hour or  (anime[2] == now.hour and anime[3] <= now.minute ) ) and anime[5] == False) :
                 os.system("notify-send " + "\"" + anime[0] + " " + str(anime[1]) + " is available since " + str(anime[2]) + ":" + str(anime[3]) + "\"")
                 anime[5] = True
         time.sleep(5*60) 
